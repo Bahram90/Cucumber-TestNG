187d1eb263068df419310d7f3642dd14b794c602
Bahram90 <brtkm90@gmail.com>
2020-04-21 21:24:29 -0400

    cucumber

--- src/test/java/CucumberExample/CukeRunner/CucumberRunner/CucumberRunner.java
+++ src/test/java/CucumberExample/CukeRunner/CucumberRunner/CucumberRunner.java
@@ -5,6 +5,14 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src/test/resources/Features",glue={"src/test/java/Steps"})
+@CucumberOptions(features="src/test/resources/Features",
+        glue={"src/test/java/Steps"},
+        dryRun = true,
+        strict = false,
+        tags = "",
+        plugin = {
+                "html:target/default-report",
+                "json:target/cucumber1.json"
+        })
 public class CucumberRunner {
 }
